77510a01ff27d894f79b48f9f9ef33fa646b48ef
Vik Goel <vgoel@live.com>
2014-08-22 20:22:46 -0400

    Initial commit

---
+++ desktop/src/me/vik/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.vik.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.vik.LD30Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD30Game(), config);
+	}
+}
